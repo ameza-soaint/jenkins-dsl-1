717eca45bcb43391b4987e558b608db336001763
Angel Meza <ang3lm3zafr33azur3@hotmail.com>
2020-12-30 16:18:16 -0300

    asdf

--- src/main/groovy/seedjobs/job_seedjobs.groovy
+++ src/main/groovy/seedjobs/job_seedjobs.groovy
@@ -4,6 +4,14 @@ def list = []
 
 def currentDir = new File(".").getAbsolutePath()
 println currentDir
+
+def currentDir2 = new File(".").getAbsolutePath().getParent()
+println currentDir2
+
+println new File(System.getProperty("user.dir")).name
+
+println new File(System.getProperty("user.dir")).parent
+
 // def dir = new File("src/main/groovy/projects")
 // dir.eachFileRecurse (FileType.FILES) { file ->
 //   list << file
