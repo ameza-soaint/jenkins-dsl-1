6d55f220db0bd27301a19cf44be29c868a9c2ba9
Angel Meza <ameza.externo@coopeuch.cl>
2021-05-10 19:43:12 -0400

    a

--- src/main/groovy/seedjobs/job_seedjobs.groovy
+++ src/main/groovy/seedjobs/job_seedjobs.groovy
@@ -9,9 +9,6 @@ println new File(getClass().protectionDomain.codeSource.location.path).parent
 
 println getClass().protectionDomain.codeSource.location.path
 
-def branchApi = new URL("https://github.com/angel-moveapps/jenkins-dsl/tree/master/src/main/groovy/projects")
-println branchApi.newReader()
-
 // def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 // branches.each {
 //     def branchName = it.name
@@ -35,7 +32,7 @@ seedjobs.each { seedjob ->
         scm {
             git {
                 remote {
-                    url('https://github.com/angel-moveapps/jenkins-dsl.git')
+                    url('https://github.com/ameza-soaint/jenkins-dsl-1.git')
                 }
                 branch('master')
                 extensions {
