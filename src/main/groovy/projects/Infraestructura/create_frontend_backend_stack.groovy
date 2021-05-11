28f79d12773532a7e0f4f071adf9bf070e429bc4
Angel Meza <ameza.externo@coopeuch.cl>
2021-05-10 21:10:02 -0400

    a

--- src/main/groovy/projects/Infraestructura/create_frontend_backend_stack.groovy
+++ src/main/groovy/projects/Infraestructura/create_frontend_backend_stack.groovy
@@ -1,3 +1,7 @@
+import jenkins.model.*
+import hudson.security.*
+
+
 // def jobDescription = "Este job utiliza una plantilla CloudFormation de un stack tipo Frontend/Backend (Client Side Render / REST API) " +
 //   "para un ambiente con recursos livianos y sin alta disponibilidad. Crea un stack que utiliza un Beanstalk junto a un API Gateway mas " +
 //   "un RDS para el backend y un Bucket S3 para el stack frontend. Ambos quedan configurados en CloudFront para recibir peticiones por Internet. " +
@@ -14,9 +18,11 @@ def job = pipelineJob('Infraestructura/create-frontend-backend-stack') {
             }
 }
 
-println "isSecurityRealmAzureAD()"
-
-helpers.Build_Job_Helper.isSecurityRealmAzureAD()
+def jenkins = Jenkins.getInstance()
+def realm = jenkins.getSecurityRealm()
+println realm.getClass().toString()
+println realm.getClass().getName()
+println realm.getClass().getSimpleName()
 
 println "inside job groovy"
 
