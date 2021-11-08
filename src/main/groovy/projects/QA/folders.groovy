c532a4da00d0d5843a5878fbe48c3fe373fa2dca
Angel Meza <ameza.externo@coopeuch.cl>
2021-11-08 18:05:21 -0300

    test

--- src/main/groovy/projects/QA/folders.groovy
+++ src/main/groovy/projects/QA/folders.groovy
@@ -1,19 +1,18 @@
-/*
+
 folder('QA') {
     displayName('QA')
     description('QA')
 	//def userIDs = helpers.Build_Job_Helper.dev_users()
-	def userIDs = ['developer']
+	// def userIDs = ['developer']
         
-            for (String oneUser : userIDs) {
-                authorization {
-                    permissions("${oneUser}", [
-                        'hudson.model.Item.Build',
-                        'hudson.model.Item.Discover',
-                        'hudson.model.Item.Cancel',
-                        'hudson.model.Item.Read'
-                    ])
-                }
-            }
+    //         for (String oneUser : userIDs) {
+    //             authorization {
+    //                 permissions("${oneUser}", [
+    //                     'hudson.model.Item.Build',
+    //                     'hudson.model.Item.Discover',
+    //                     'hudson.model.Item.Cancel',
+    //                     'hudson.model.Item.Read'
+    //                 ])
+    //             }
+    //         }
 }
-*/
